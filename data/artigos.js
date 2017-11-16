b3b63ceeb35e566beb7e2dd4e887e51c20b96691
Arthur Nishikawa <nishikawa.poli@gmail.com>
2017-11-15 22:50:01 -0200

    delete all

--- data/artigos.js
+++
@@ -1,7 +0,0 @@
-{
-	"titulo": {
-		"pt": "Publica&ccedil;&otilde;es em peri&oacute;dicos",
-		"en": "Publications",
-		"es": "Publicaciones"
-	}
-}
--- old/js/posicoes.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-	$.getJSON(path + "data/posicoes.js", function(data) {
-		$("#titulo").append(mudaIdioma(idioma,data.titulo));
-		$("#conteudo").append(mudaIdioma(idioma,data.conteudo));
-	});
-});
